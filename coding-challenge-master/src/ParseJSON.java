d28d801c1768d4bca21cb9473b68d4a57e10e085
Calvin <calvinliu411@gmail.com>
2016-03-31 20:21:42 -0700

    Finished processing 1 tweet

--- coding-challenge-master/src/ParseJSON.java
+++ coding-challenge-master/src/ParseJSON.java
@@ -8,7 +8,8 @@ import org.json.simple.parser.JSONParser;
 public class ParseJSON {
 	private ArrayList<String> created_at;
 	private Set<String> hashtags;
-	private Map<String, String> dictionary;
+    private Set<String> aux_hashtags;
+	private Map<String, List<String>> dictionary;
 	private JSONParser parser;
 	private boolean found = false;
 
@@ -17,8 +18,9 @@ public class ParseJSON {
 	public ParseJSON() {
 		created_at = new ArrayList<String>();
 		hashtags = new HashSet<String>();
+        aux_hashtags = new HashSet<String>();
 		parser = new JSONParser();
-		dictionary = new HashMap<String, String>();
+		dictionary = new HashMap<String, List<String>>();
 	}
 
 	public void read(FileReader file) throws IOException {
@@ -33,18 +35,36 @@ public class ParseJSON {
 
 			created_at.add(single_created_at);
 			Iterator<JSONObject> iterator = single_hash_tag.iterator();
+            //Check all the hashtags in the hashtag field
 			while(iterator.hasNext()) {
-				String hash_text = (String)iterator.next().get("text");
-				if(hashtags.contains(hash_text)) { //If it is already in the graph
-					
-				}
-			}
+                String hash_text = (String) iterator.next().get("text");
+                aux_hashtags.add(hash_text);
+            }
+
+            for(String s : aux_hashtags) {
+                List<String> listOfTags = new ArrayList<String>();
+                for(String t : aux_hashtags) {
+                    if(s != t) {
+                        listOfTags.add(t);
+                    }
+                }
+                dictionary.put(s, listOfTags);
+            }
+
+            aux_hashtags = null;
+
+            averageEdges(dictionary, hashtags);
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	public double averageEdges(HashMap<String, String> nodes, HashSet<String> uniqueHashes) {
-		return  nodes.size() / uniqueHashes.size();
+	public double averageEdges(HashMap<String, List<String>> nodes, HashSet<String> uniqueHashes) {
+        int total_edges = 0;
+        for(List<String> value : nodes.values()) {
+           total_edges += value.size();
+        }
+		return  total_edges / uniqueHashes.size();
 	}
 }
